a736eeb4da7031d392cf24d84cf025b4ab169673
zto-michishita <j-michishita@croooober.co.jp>
2021-05-07 15:40:18 +0900

    [FIX] 一旦シンプルになるようにテーブルを変更

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -3,13 +3,14 @@
 # --- !Ups
 
 create table users (
-  id                        bigint not null,
-  name                      varchar(255)
-  text                      varchar(500),
-  introduced                timestamp,
-  discontinued              timestamp,
-  constraint pk_users      primary key (id))
-;
+    id int auto_increment primary key,
+    name varchar(255) not null,
+    text varchar(500) not null,
+);
+
+insert into users (id,name,text) values (default, '名無し', 'サンプルテキストです。');
+insert into users (id,name,text) values (default, '名無し', 'サンプルテキスト２です。');
 
 # --- !Downs
+
 drop table users;
