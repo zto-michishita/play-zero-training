366880070787547f405dc6224c060662b7db0cbc
zto-michishita <j-michishita@croooober.co.jp>
2021-05-07 15:35:30 +0900

    [UPDATE] テーブル構成を変更、使っていないライブラリを削除

--- app/models/Users.java
+++ app/models/Users.java
@@ -2,22 +2,19 @@ package models;
 
 import io.ebean.Model;
 
-import play.data.format.Formats;
 import play.data.validation.Constraints;
 
+import javax.persistence.Id;
 import javax.persistence.Entity;
-import java.util.Date;
 
 @Entity
 public class Users extends Model {
-    private static final long serialVersionUID = 1L;
-
+    @Id
+    public Integer id;
+    
     @Constraints.Required
     public String name;
-    
-    @Formats.DateTime(pattern="yyyy-MM-dd")
-    public Date introduced;
-    
-    @Formats.DateTime(pattern="yyyy-MM-dd")
-    public Date discontinued;
+
+    @Constraints.Required
+    public String text;
 }
