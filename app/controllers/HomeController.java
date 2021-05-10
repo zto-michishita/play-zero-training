fb97d5f9b215c3608efbd53436fd8468fb925a6b
zto-michishita <j-michishita@croooober.co.jp>
2021-05-10 16:45:18 +0900

    [UPDATE] データベースから値を取ってくるメソッドを作成

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -2,23 +2,11 @@ package controllers;
 
 import play.mvc.*;
 
-import io.ebean.*;
 import java.util.List;
 
-import models.Users;
+import models.User;
 
-/**
- * This controller contains an action to handle HTTP requests
- * to the application's home page.
- */
 public class HomeController extends Controller {
-
-    /**
-     * An action that renders an HTML page with a welcome message.
-     * The configuration in the <code>routes</code> file means that
-     * this method will be called when the application receives a
-     * <code>GET</code> request with a path of <code>/</code>.
-     */
     public Result index() {
         return ok(views.html.index.render());
     }
@@ -27,9 +15,8 @@ public class HomeController extends Controller {
         return ok(views.html.hello.render(massage));
     }
 
-    public Result database() {
-        List<Users> user = Ebean.find(Users.class).findList();
-
-        return ok(views.html.database.render(user));
+    public Result board() {        
+        List<User> users = User.finder.all();
+        return ok(views.html.board.render(users));
     }
 }
