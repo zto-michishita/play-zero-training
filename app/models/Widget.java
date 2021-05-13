8a63d206a2beccf906a1c03280826ad41684c01c
zto-michishita <j-michishita@croooober.co.jp>
2021-05-13 18:14:23 +0900

    [UPDATE] idの表示をさせるために変数を追加

--- app/models/Widget.java
+++ app/models/Widget.java
@@ -1,10 +1,12 @@
 package models;
 
 public class Widget {
+    public Long id;
     public String name;
     public String text;
 
-    public Widget(String name, String text) {
+    public Widget(Long id, String name, String text) {
+        this.id = id;
         this.name = name;
         this.text = text;
     }
