69eb23c8922aea85415dc37d10c9237933af084a
zto-michishita <j-michishita@croooober.co.jp>
2021-05-13 18:13:05 +0900

    [UPDATE] DeleteControllerの内容をFormControllerに移した

--- app/controllers/FormController.java
+++ app/controllers/FormController.java
@@ -24,6 +24,8 @@ import static play.libs.Scala.asScala;
 @Singleton
 public class FormController extends Controller {
     private final Form<WidgetData> form;
+    private final Form<DeleteFormData> deleteForm;
+
     private MessagesApi messagesApi;
     private final List<Widget> widgets;
     private List<User> users;
@@ -34,15 +36,17 @@ public class FormController extends Controller {
     public FormController(FormFactory formFactory, MessagesApi messagesApi) {
         this.users = User.finder.all();
         this.form = formFactory.form(WidgetData.class);
+        this.deleteForm = formFactory.form(DeleteFormData.class);
         this.messagesApi = messagesApi;
         this.widgets = Lists.newArrayList();
         users.forEach(data -> {
-            widgets.add(new Widget(data.name, data.text));
+            widgets.add(new Widget(data.id, data.name, data.text));
         });
+        
     }
 
     public Result showForm(Http.Request request) {
-        return ok(views.html.board.render(asScala(widgets), form, request, messagesApi.preferred(request)));
+        return ok(views.html.board.render(asScala(widgets), deleteForm, form, request, messagesApi.preferred(request)));
     }
 
     public Result create(Http.Request request) {
@@ -50,16 +54,30 @@ public class FormController extends Controller {
 
         if (boundForm.hasErrors()) {
             logger.error("errors = {}", boundForm.errors());
-            return badRequest(views.html.board.render(asScala(widgets), boundForm, request, messagesApi.preferred(request)));
+            return badRequest(views.html.board.render(asScala(widgets), deleteForm, boundForm, request, messagesApi.preferred(request)));
         } else {
             WidgetData data = boundForm.get();
             User addUser = new User();
             addUser.setName(data.getName());
             addUser.setText(data.getText());
             Ebean.save(addUser);
-            widgets.add(new Widget(addUser.getName(), addUser.getText()));
+            widgets.add(new Widget(addUser.getId() ,addUser.getName(), addUser.getText()));
             users.add(addUser);
             return redirect(routes.FormController.showForm()).flashing("info", "書き込みました");
+        }
+    }
+
+    public Result delete(Http.Request request) {
+        final Form<DeleteFormData> boundForm = deleteForm.bindFromRequest(request);
+
+        if (boundForm.hasErrors()) {
+            logger.error("errors = {}", boundForm.errors());
+            return badRequest(views.html.board.render(asScala(widgets), boundForm, form, request, messagesApi.preferred(request)));
+        } else {
+            DeleteFormData data = boundForm.get();
+            User.find.ref(data.id).delete();
+            widgets.subList(data.id).clear();
+            return redirect(routes.deleteFormController.showForm()).flashing("info", "削除しました");
         }
     }
 
