d8669524c2d85d6fa1031d746f639dde65bc84ac
zto-michishita <j-michishita@croooober.co.jp>
2021-05-17 15:02:57 +0900

    [UPDATE] Updateに対応した

--- app/controllers/FormController.java
+++ app/controllers/FormController.java
@@ -26,10 +26,11 @@ import static play.libs.Scala.asScala;
 public class FormController extends Controller {
     private final Form<CreateForm> createForm;
     private final Form<DeleteForm> deleteForm;
-    private final Form<UpdateForm> updateForm
+    private final Form<UpdateForm> updateForm;
 
     private MessagesApi messagesApi;
     private List<User> users;
+    private User updateUser;
 
     private final Long master = 0l;
 
@@ -39,12 +40,18 @@ public class FormController extends Controller {
     public FormController(FormFactory formFactory, MessagesApi messagesApi) {
         this.createForm = formFactory.form(CreateForm.class);
         this.deleteForm = formFactory.form(DeleteForm.class);
+        this.updateForm = formFactory.form(UpdateForm.class);
         this.messagesApi = messagesApi;
     }
 
     public Result showForm(Http.Request request) {
         this.users = User.finder.all();
         return ok(views.html.board.render(users, deleteForm, createForm, request, messagesApi.preferred(request)));
+    }
+
+    public Result fixForm(Http.Request request, Long id) {
+        this.updateUser = User.finder.byId(id);
+        return ok(views.html.fix.render(users, updateForm, request, messagesApi.preferred(request)));
     }
 
     public Result create(Http.Request request) {
@@ -81,10 +88,12 @@ public class FormController extends Controller {
 
         if (boundForm.hasErrors()) {
             logger.error("errors = {}", boundForm.errors());
-            return badRequest(views.html.board.render(users, boundForm, createForm, request, messagesApi.preferred(request)));
+            return badRequest(views.html.fix.render(users, boundForm, request, messagesApi.preferred(request)));
         } else {
             UpdateForm data = boundForm.get();
-            User.finder.ref(data.getId()).delete();
+            this.updateUser.setName(data.getName());
+            this.updateUser.setText(data.getText());
+            Ebean.save(this.updateUser);
             return redirect(routes.FormController.showForm()).flashing("info", "修正しました");
         }
     }
