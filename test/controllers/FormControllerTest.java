488677e12f909c5505a440e4686d745ca7a51936
zto-michishita <j-michishita@croooober.co.jp>
2021-05-24 18:54:27 +0900

    [DAY STATE] fakeRequestに書き換えて、nullで合った場合のテストを追加

--- test/controllers/FormControllerTest.java
+++ test/controllers/FormControllerTest.java
@@ -5,15 +5,20 @@ import play.Application;
 import play.inject.guice.GuiceApplicationBuilder;
 import play.mvc.Http;
 import play.mvc.Result;
+import play.test.Helpers;
 import play.test.WithApplication;
 import com.google.common.collect.ImmutableMap;
 
+import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertEquals;
+import static org.junit.matchers.JUnitMatchers.containsString;
 import static play.mvc.Http.Status.OK;
 import static play.mvc.Http.Status.SEE_OTHER;
 import static play.test.Helpers.GET;
 import static play.test.Helpers.POST;
+import static play.test.Helpers.BAD_REQUEST;
 import static play.test.Helpers.route;
+import static play.test.Helpers.*;
 
 public class FormControllerTest extends WithApplication {
 
@@ -24,7 +29,7 @@ public class FormControllerTest extends WithApplication {
 
     @Test
     public void fixページが表示できるかどうか() {
-        Http.RequestBuilder request = new Http.RequestBuilder()
+        Http.RequestBuilder request = Helpers.fakeRequest()
                 .method(GET)
                 .uri("/board/fix?id=1");
 
@@ -34,7 +39,7 @@ public class FormControllerTest extends WithApplication {
 
     @Test
     public void 掲示板が表示出来るかどうか() {
-        Http.RequestBuilder request = new Http.RequestBuilder()
+        Http.RequestBuilder request = Helpers.fakeRequest()
                 .method(GET)
                 .uri("/board");
 
@@ -44,7 +49,7 @@ public class FormControllerTest extends WithApplication {
 
     @Test
     public void 投稿が出来るかどうか() {
-        Http.RequestBuilder request = new Http.RequestBuilder()
+        Http.RequestBuilder request = Helpers.fakeRequest()
                 .method(POST)
                 .bodyForm(ImmutableMap.of("name","投稿するよ", "text", "投稿"))
                 .uri("/board/create");
@@ -54,8 +59,38 @@ public class FormControllerTest extends WithApplication {
     }
 
     @Test
+    public void 投稿のFormにnullがあったらエラーが返るかどうか() {
+        String errorText = "必須入力です";
+
+        try {
+            Http.RequestBuilder request = Helpers.fakeRequest()
+                .method(POST)
+                .bodyForm(ImmutableMap.of("name","投稿するよ", "text", ""))
+                .uri("/board/create");
+
+            Result result = route(app, request);
+            result.status();
+        } catch (IllegalStateException e) {
+            assertThat(e.getMessage(),containsString(errorText));
+        }
+
+        try {
+            Http.RequestBuilder requestTextNull = new Http.RequestBuilder()
+                .method(POST)
+                .bodyForm(ImmutableMap.of("name","TextがNull", "text", ""))
+                .uri("/board/create");
+
+            Result result = route(app, requestTextNull);
+            result.status();
+        } catch (IllegalStateException e) {
+            assertThat(e.getMessage(),containsString(errorText));
+        }
+
+    }
+
+    @Test
     public void 投稿が削除出来るかどうか() {
-        Http.RequestBuilder request = new Http.RequestBuilder()
+        Http.RequestBuilder request = Helpers.fakeRequest()
                 .method(POST)
                 .bodyForm(ImmutableMap.of("id","1"))
                 .uri("/board/delete");
@@ -66,13 +101,30 @@ public class FormControllerTest extends WithApplication {
 
     @Test
     public void 投稿の編集が出来るかどうか() {
-        Http.RequestBuilder request = new Http.RequestBuilder()
+        Http.RequestBuilder request = Helpers.fakeRequest()
                 .method(POST)
                 .bodyForm(ImmutableMap.of("name","編集するよ", "text", "編集"))
                 .uri("/board/update");
 
         Result result = route(app, request);
         assertEquals(SEE_OTHER, result.status());
+    }
+
+    @Test
+    public void 投稿の編集Formにnullがあったらエラーが返るかどうか() {
+        String errorText = "必須入力です";
+        try {
+            Http.RequestBuilder request = Helpers.fakeRequest()
+                .method(POST)
+                .bodyForm(ImmutableMap.of("name","テキストがnull", "text", ""))
+                .uri("/board/update");
+
+            Result result = route(app, request);
+            result.status();
+        } catch (IllegalStateException e) {
+            assertThat(e.getMessage(),containsString(errorText));
+        }
+
     }
 
 }
